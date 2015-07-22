6eeb50934855bcee20c580339fef6643dde51c44
Yogesh Nagarur <hi@yogeshn.com>
2015-07-22 18:34:22 +0100

    first commit

---
+++ speakers.bat
@@ -0,0 +1,3 @@
+@ECHO OFF 
+C:\NIRCMD\NIRCMDC setdefaultsounddevice "Speakers" 1
+C:\NIRCMD\NIRCMDC setdefaultsounddevice "Speakers" 2
---
+++ tv.bat
@@ -0,0 +1,3 @@
+@ECHO OFF 
+C:\NIRCMD\NIRCMDC setdefaultsounddevice "TV" 1
+C:\NIRCMD\NIRCMDC setdefaultsounddevice "TV" 2
